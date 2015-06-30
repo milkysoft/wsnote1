7b78f26495b22ba815538adbe51ec799b798c5a8
zuoqin <zuoqinr@163.com>
2015-06-30 14:23:36 +0800

    Database settings

--- Application3/Database.fs
+++ Application3/Database.fs
@@ -14,9 +14,9 @@ module Database =
         //let connectionString = """Data Source=(LocalDB)\v11.0;AttachDbFilename="C:\Users\User\Documents\Visual Studio 2013\Projects\Blog1\ConsoleApplication1\DatabaseBlog.mdf";Integrated Security=True"""        
         //let connectionString = """Data Source=(LocalDB)\v11.0;AttachDbFilename="E:\1\DatabaseBlog.mdf";Integrated Security=True"""
         //let connectionString = "Server=e760f0f2-fdd9-4689-a929-a4c5009f9e57.sqlserver.sequelizer.com;Database=dbe760f0f2fdd94689a929a4c5009f9e57;User Id=wqycsdtupynecfpw;Password=p786tkkPTzgsXgUtDsTXQ5KCoYhDqMByfwUEZSFQBrtsaA7DmmNUAWksuDRCdPyf;"
-        //let connectionString = "Server=52.74.177.104\SQLEXPRESS;Database=blog;User Id=sa;Password=1!password;"
+        let connectionString = "Server=52.74.177.104\SQLEXPRESS;Database=blog;User Id=sa;Password=1!password;"
         //let connectionString = "Server=dev.t5p.hk;Database=testdb;User Id=zuoqin;Password=Qwerty123;"
-        let connectionString = "Server=TF5ALEXEYZUO\SQLSRV2012;Database=trunk;User Id=sa;Password=1!password;"
+        //let connectionString = "Server=TF5ALEXEYZUO\SQLSRV2012;Database=trunk;User Id=sa;Password=1!password;"
         type GetBlog = SqlCommandProvider<"SELECT * FROM Post" , connectionString>
 
         type DeletePost = SqlCommandProvider<"DELETE FROM Post WHERE Id=@id" , connectionString>
